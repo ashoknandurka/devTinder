a95de9861d4a2978b577fbd724e5e11e2d48adf2
ashokkumar-nandurka_mars <ashokkumar.nandurka@effem.com>
2025-09-16 23:07:47 +0530

    expore get, post and delete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 7777
+const express = require("express");
+const app = express();
+const port = 7777;
 
-app.get('/test', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/user", (req, res) => {
+  res.send({ firstName: "John", lastName: "Doe" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("user posted successfully !");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("user deleted successfully !");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
